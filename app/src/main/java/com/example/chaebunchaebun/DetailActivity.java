b22e0aaa90973f00ee86d4e3faeeff9c48cb8965
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-05-29 23:25:15 +0900

    Comment_1

--- app/src/main/java/com/example/chaebunchaebun/DetailActivity.java
+++ app/src/main/java/com/example/chaebunchaebun/DetailActivity.java
@@ -1,23 +1,47 @@
 package com.example.chaebunchaebun;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DetailActivity extends AppCompatActivity {
+    private FirebaseFirestore mDataBase;
     ListView comment_list;
     View header, footer;
     EditText comment_edit;
+    String nickname = "";
+    private static final String TAG = "user";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {//DB
+        //DB
+        mDataBase = FirebaseFirestore.getInstance();
+        footer = getLayoutInflater().inflate(R.layout.detail_page_footer, null, false);
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_page);
 
@@ -47,5 +71,67 @@ public class DetailActivity extends AppCompatActivity {
     private void setFooter(){
         comment_edit = (EditText)footer.findViewById(R.id.footer_edit);
         Button comment_btn = (Button)footer.findViewById(R.id.footer_btn);
+
+        String getComment = comment_edit.getText().toString();
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date(System.currentTimeMillis()));
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("ID");
+
+        comment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //private void setContent() {
+                //getNickname(id);
+
+                System.out.println("getComment: "+ getComment);
+                Map<String, Object> result = new HashMap<>();
+                result.put("comment", getComment);
+                result.put("time", time);
+                //result.put("nickname", nickname);
+                System.out.println(result);
+
+                mDataBase.collection("comments")
+                        .document(time)
+                        .set(result)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(DetailActivity.this, "저장되었습니다", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(DetailActivity.this, "저장에 실패했습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+    }
+    private void getNickname(String getUserId) {
+        mDataBase.collection("users")
+                .whereEqualTo("userId", getUserId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + " => " + document.getData().get("userPw"));
+                                String userNickname = document.getData().get("userNickname").toString();
+                                Log.d(TAG, userNickname);
+                                setNickname(userNickname);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 }
