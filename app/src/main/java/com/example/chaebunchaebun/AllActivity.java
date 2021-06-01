3c568035c923e757da1cf842fbca1217f93a9025
ahnsumin <sherri98@naver.com>
2021-06-01 20:03:12 +0900

    전체 소분 페이지는 뜨는데 리스트는 안 떠 왜일까

--- app/src/main/java/com/example/chaebunchaebun/AllActivity.java
+++ app/src/main/java/com/example/chaebunchaebun/AllActivity.java
@@ -9,27 +9,44 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AllActivity extends AppCompatActivity{
     NavigationView navigationView;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle barDrawerToggle;
+    private ListView mListView;
+    private FirebaseFirestore mDataBase;
+    private static final String TAG = "user";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((R.layout.allpage));
+
+        mDataBase = FirebaseFirestore.getInstance();
+
+        mListView = (ListView) findViewById(R.id.listView);
 //네비게이션 시작
         navigationView=findViewById(R.id.nav);
         drawerLayout=findViewById(R.id.layout_drawer);
         //item icon색조를 적용하지 않도록.. 설정 안하면 회색 색조
         navigationView.setItemIconTintList(null);
 
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("ID");
 
         //네비게이션뷰의 아이템을 클릭했을때
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -38,26 +55,32 @@ public class AllActivity extends AppCompatActivity{
                 switch(item.getItemId()){
                     case R.id.menu_home:
                         Intent home = new Intent(getApplicationContext(), HomeActivity.class);
+                        home.putExtra("ID", id);
                         startActivity(home);
                         break;
                     case R.id.menu_writing:
                         Intent writing = new Intent(getApplicationContext(), WritingActivity.class);
+                        writing.putExtra("ID", id);
                         startActivity(writing);
                         break;
                     case R.id.menu_all:
                         Intent all = new Intent(getApplicationContext(), AllActivity.class);
+                        all.putExtra("ID", id);
                         startActivity(all);
                         break;
                     case R.id.menu_partici:
                         Intent partici = new Intent(getApplicationContext(), PartiActivity.class);
+                        partici.putExtra("ID", id);
                         startActivity(partici);
                         break;
                     case R.id.menu_lead:
                         Intent lead = new Intent(getApplicationContext(), LeadActivity.class);
+                        lead.putExtra("ID", id);
                         startActivity(lead);
                         break;
                     case R.id.menu_membership:
                         Intent membership = new Intent(getApplicationContext(), MembershipActivity.class);
+                        membership.putExtra("ID", id);
                         startActivity(membership);
                         break;
                     case R.id.menu_logout:
@@ -86,6 +109,7 @@ public class AllActivity extends AppCompatActivity{
         //삼선 아이콘과 화살표아이콘이 자동으로 변환하도록
         drawerLayout.addDrawerListener(barDrawerToggle);
 
+        dataSetting();
     }//onCreate method..
 
     //액션바의 메뉴를 클릭하는 이벤트를 듣는
@@ -118,4 +142,30 @@ public class AllActivity extends AppCompatActivity{
         return true;
     }
 
+    private void dataSetting() {
+        MyAdapter myAdapter = new MyAdapter();
+
+        mDataBase.collection("market")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                String s_count = document.getData().get("count").toString();
+                                int count = Integer.parseInt(s_count);
+                                String title = document.getData().get("title").toString();
+                                String nickname = document.getData().get("nickname").toString();
+                                String location = document.getData().get("location").toString();
+
+                                myAdapter.addItem(count, title, nickname, location);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+        mListView.setAdapter(myAdapter);
+    }
 }
---
+++ app/src/main/java/com/example/chaebunchaebun/MyItem.java
@@ -0,0 +1,40 @@
+package com.example.chaebunchaebun;
+
+public class MyItem {
+    private int count = 0;
+    private String title = "";
+    private String nickname = "";
+    private String location = "";
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNickname() {
+        return title;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
