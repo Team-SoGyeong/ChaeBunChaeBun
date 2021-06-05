1b75cb9ebcfed31ee8b3a6e610541eb77b73cdb5
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-06-06 00:10:20 +0900

    end of withdrawalpage

--- app/src/main/java/com/example/chaebunchaebun/WithdrawActivity.java
+++ app/src/main/java/com/example/chaebunchaebun/WithdrawActivity.java
@@ -2,30 +2,64 @@ package com.example.chaebunchaebun;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class WithdrawActivity extends AppCompatActivity {
+    private FirebaseFirestore mDataBase;
+    private static final String TAG = "user";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((R.layout.withdrawal_membership));
+
+        mDataBase = FirebaseFirestore.getInstance();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         Button withdraw = (Button) findViewById(R.id.btn_withdraw);
 
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("ID");
+
         withdraw.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast tMsg = Toast.makeText(WithdrawActivity.this, "탈퇴되셨습니다.", Toast.LENGTH_SHORT);
-                tMsg.show();
+                System.out.println(id);
+                mDataBase.collection("users")
+                        .document(id)
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(WithdrawActivity.this, "탈퇴되셨습니다.", Toast.LENGTH_SHORT).show();
+                                Intent first = new Intent(getApplicationContext(), SignInActivity.class);
+                                startActivity(first);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error deleting document", e);
+                            }
+                        });
             }
         });
+
 
     }
 }
