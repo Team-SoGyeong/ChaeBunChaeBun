b61101e6a931ac824369ad5479c8c62a6d8c9f25
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-08-01 02:53:14 +0900

    내 정보 xml, 페이지 연결 오류 해결

--- app/src/main/java/com/example/chaebunchaebun/MypageFragment.java
+++ app/src/main/java/com/example/chaebunchaebun/MypageFragment.java
@@ -1,5 +1,6 @@
 package com.example.chaebunchaebun;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,35 +19,16 @@ import org.w3c.dom.Text;
  */
 public class MypageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    TextView MyProfile, MyStory, MyComment, MyFavorite, MyQuestion, MyShare;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private ProfileFragment profile;
 
     public MypageFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MypageFragment newInstance(String param1, String param2) {
         MypageFragment fragment = new MypageFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,18 +36,30 @@ public class MypageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
+        View view = null;
+        view = inflater.inflate(R.layout.fragment_mypage, null);
+
+        MyProfile = (TextView) view.findViewById(R.id.my_profile);
+        MyStory = (TextView) view.findViewById(R.id.my_story);
+        MyComment = (TextView) view.findViewById(R.id.my_comment);
+        MyFavorite = (TextView) view.findViewById(R.id.my_favorite);
+        MyQuestion = (TextView) view.findViewById(R.id.my_question);
+        MyShare = (TextView) view.findViewById(R.id.my_share);
+
+        MyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ProfileActivity.class));
+            }
+        });
+
+        return view;
     }
+
 }
