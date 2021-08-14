aafeb2e7c218e6d9aab7e5c3190fb076ecbd1975
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-08-15 03:06:10 +0900

    writing detail xml (1)

--- app/src/main/java/com/example/chaebunchaebun/WritingChaebunActivity.java
+++ app/src/main/java/com/example/chaebunchaebun/WritingChaebunActivity.java
@@ -3,13 +3,20 @@ package com.example.chaebunchaebun;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WritingChaebunActivity extends AppCompatActivity {
     ImageView back, writing, btn_back;
+    Spinner date_spinner, amount_spinner;
+    String date_arr[] = {"1일 전", "2일 전", "3일 전", "일주일 이내", "2주 이내"};
+    String amount_arr[] ={"kg","g","개"};
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,44 @@ public class WritingChaebunActivity extends AppCompatActivity {
         writing = (ImageView) findViewById(R.id.btn_next);
         btn_back = (ImageView) findViewById(R.id.btn_back);
 
+        date_spinner = (Spinner) findViewById(R.id.date_spinner2);
+        amount_spinner = (Spinner) findViewById(R.id.amount_spinner2);
+
+        //스피너 이벤트
+        ArrayAdapter<String> date_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, date_arr);
+        date_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        date_spinner.setAdapter(date_adapter);
+
+        date_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String str = date_arr[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> amount_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, amount_arr);
+        amount_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        amount_spinner.setAdapter(amount_adapter);
+
+        amount_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String amount_str = amount_arr[position];
+                Toast.makeText(WritingChaebunActivity.this,"선택 단위:"+amount_str,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //버튼 이벤트
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,4 +85,5 @@ public class WritingChaebunActivity extends AppCompatActivity {
             }
         });
     }
+
 }
