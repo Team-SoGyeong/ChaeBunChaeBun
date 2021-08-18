4251a27ac2261ac6f0b884c9b46f3be34e84bc55
ahnsumin <sherri98@naver.com>
2021-08-19 01:52:05 +0900

    글 작성 post 완료

--- app/src/main/java/com/example/chaebunchaebun/PostTask.java
+++ app/src/main/java/com/example/chaebunchaebun/PostTask.java
@@ -30,20 +30,21 @@ public class PostTask extends AsyncTask<String, Void, Void> {
         try {
             URL url = new URL("http://3.37.243.188:8080/" + params[0]);
             conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000);
+            conn.setReadTimeout(60000);
             conn.setConnectTimeout(15000);
             conn.setRequestMethod("POST");
             conn.setDoInput(true); // InputStream으로 서버로 부터 응답을 받겠다는 옵션.
             conn.setDoOutput(true); // OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
             conn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
-            conn.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Connection", "Keep-Alive");
 
             OutputStream os = conn.getOutputStream();
             os.write(params[1].getBytes("UTF-8"));
             os.flush();
             os.close();
 
-            BufferedReader br = null;
+            //BufferedReader br = null;
             if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
                 BufferedReader bufferedReader = new BufferedReader(tmp);
@@ -55,9 +56,19 @@ public class PostTask extends AsyncTask<String, Void, Void> {
                 Log.i("receiveMsg: ", receiveMsg);
 
                 bufferedReader.close();
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             } else {
-                br = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+                InputStreamReader tmp = new InputStreamReader(conn.getErrorStream(), "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+                while ((str = bufferedReader.readLine()) != null){
+                    buffer.append(str);
+                }
+                receiveMsg = buffer.toString();
+                Log.i("receiveMsg: ", receiveMsg);
+
+                bufferedReader.close();
+//                br = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
             }
         }  catch (Exception e) {
             e.printStackTrace();
