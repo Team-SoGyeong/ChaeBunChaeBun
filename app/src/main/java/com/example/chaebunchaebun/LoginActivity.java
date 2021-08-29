ad0539d2ab393bda770877b072a9c902a94be6c9
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-08-29 23:24:43 +0900

    소셜 로그인_EX1 xml

--- app/src/main/java/com/example/chaebunchaebun/LoginActivity.java
+++ app/src/main/java/com/example/chaebunchaebun/LoginActivity.java
@@ -1,21 +1,59 @@
 package com.example.chaebunchaebun;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class LoginActivity extends AppCompatActivity {
-    ImageView signIn, signUp, fogotPW;
+    private static final String TAG = "사용자";
+    ImageView signIn, signUp, loginV1, set_invaild;
+    private SessionCallback sessionCallback = new SessionCallback();
+    Session session;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_start);
 
+        loginV1 = (ImageButton) findViewById(R.id.loginV1);
         signIn = (ImageView) findViewById(R.id.sign_in);
         signUp = (ImageView) findViewById(R.id.sign_up);
+        set_invaild = (ImageView) findViewById(R.id.set_invaild);
+
+        session = Session.getCurrentSession();
+        session.addCallback(sessionCallback);
+
+        Intent intent = new Intent(getBaseContext(), NavigationActivity.class);
+
+        loginV1.setOnClickListener(v -> {
+            if (Session.getCurrentSession().checkAndImplicitOpen()) {
+                Log.d(TAG, "onClick: 로그인 세션살아있음");
+                // 카카오 로그인 시도 (창이 안뜬다.)
+                sessionCallback.requestMe();
+            } else {
+                Log.d(TAG, "onClick: 로그인 세션끝남");
+                // 카카오 로그인 시도 (창이 뜬다.)
+                session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
+                startActivity(intent);
+            }
+        });
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +70,63 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        set_invaild.setOnClickListener(v -> {
+            Log.d(TAG, "onCreate:click ");
+            UserManagement.getInstance()
+                    .requestLogout(new LogoutResponseCallback() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            super.onSessionClosed(errorResult);
+                            Log.d(TAG, "onSessionClosed: "+errorResult.getErrorMessage());
+
+                        }
+                        @Override
+                        public void onCompleteLogout() {
+                            if (sessionCallback != null) {
+                                Session.getCurrentSession().removeCallback(sessionCallback);
+                            }
+                            Log.d(TAG, "onCompleteLogout:logout ");
+                        }
+                    });
+        });
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public String getKeyHash(){
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            if(packageInfo == null) return null;
+            for(Signature signature: packageInfo.signatures){
+                try{
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return android.util.Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                }catch (NoSuchAlgorithmException e){
+                    Log.w("getKeyHash", "Unable to get MessageDigest. signature="+signature, e);
+                }
+            }
+        }catch(PackageManager.NameNotFoundException e){
+            Log.w("getPackageInfo", "Unable to getPackageInfo");
+        }
+        return null;
     }
 }
